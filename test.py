42de3a0a5a701d7ae95fd054250b67585e4925d0
Andy Kenney <a.kenney@utoronto.ca>
2023-09-15 19:09:02 -0400

    added downloaded filter fucntion

--- test.py
+++ test.py
@@ -6,8 +6,15 @@ import pandas as pd
 import streamlit as st
 from streamlit_folium import folium_static
 
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
 
-st.write("Test")
+
+st.header("Priorities in the South Nation Watershed")
 
 #get the share link for the data file form one drive and past below
 onedrive_link = 'https://1drv.ms/u/s!Alu-nJHZ-vTw8wUirrIMsP5SxVPS?e=nyljiN'
@@ -32,23 +39,103 @@ def getData():
 gdf = getData()
 select_gdf = gdf
 
-with st.form('Filter Data'):
+# with st.form('Filter Data'):
 
-    param = st.selectbox("Select a parameter for your query.", gdf.columns)
-    submitted = st.form_submit_button("Submit")
+#     param = st.selectbox("Select a parameter for your query.", gdf.columns)
+#     submitted = st.form_submit_button("Submit")
     
-    if submitted:
-        st.write("Your data will be filtered by " + param)
+#     if submitted:
+#         st.write("Your data will be filtered by " + param)
 
-with st.form('Filter Data2'):
+# with st.form('Filter Data2'):
 
-    val = st.selectbox('Select a value.', gdf[param].unique())
+#     val = st.selectbox('Select a value.', gdf[param].unique())
     
-    submitted = st.form_submit_button("Submit")
+#     submitted = st.form_submit_button("Submit")
     
-    if submitted:
-        st.write("You are filtering " + param + " by " + val)
-        select_gdf = gdf.loc[gdf[param] == val]
+#     if submitted:
+#         st.write("You are filtering " + param + " by " + val)
+#         select_gdf = gdf.loc[gdf[param] == val]
+
+
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
+
+
+filtdf = filter_dataframe(gdf) 
 
 
 def mapIt(df):
@@ -60,4 +147,4 @@ def mapIt(df):
 
     folium_static(m)
 
-mapIt(select_gdf)
+mapIt(filtdf)
