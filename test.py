e2f5045839875b2ffc18331913a07be9d5446fef
Andy Kenney <a.kenney@utoronto.ca>
2023-09-15 09:57:47 -0400

    add AgGrid

--- test.py
+++ test.py
@@ -5,6 +5,11 @@ import folium
 from folium.plugins import Fullscreen
 from streamlit_folium import folium_static
 
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import JsCode
+from st_aggrid.shared import GridUpdateMode
+
 st.write("Test")
 
 #get the share link for the data file form one drive and past below
@@ -23,6 +28,37 @@ def load_onedrive (onedrive_link):
 fileName = load_onedrive(onedrive_link)
 
 gdf = gpd.read_file(fileName)
+
+AgGrid(gdf)
+
+
+# def aggFilter(agData):
+    
+#     """ This function sets up the grid to view and filter the data"""
+    
+#     df = agData.copy()
+
+#     gb = GridOptionsBuilder.from_dataframe(df)
+#     gb.configure_pagination(enabled=True)
+#     gb.configure_default_column(editable=True, filter=True)
+
+
+#     gridOptions = gb.build()
+
+#     gridReturn = AgGrid(df,
+#         gridOptions=gridOptions,
+#         allow_unsafe_jscode=True,
+#         height = 500, 
+#         theme = 'streamlit',
+#         enable_enterprise_modules=True, # enables right click and fancy features - can add license key as another parameter (license_key='string') if you have one
+#         key='select_grid', # stops grid from re-initialising every time the script is run
+#         reload_data=True, # allows modifications to loaded_data to update this same grid entity
+#         update_mode=GridUpdateMode.MANUAL,
+#         data_return_mode="FILTERED_AND_SORTED")
+
+#     gridReturnData = gridReturn['data']
+
+
 
 m = folium.Map(location = (45.404028, -75.544722), zoom_start = 12)
 
