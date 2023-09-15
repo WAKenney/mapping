c8db8c19bd16f0dc3f99e0e8c49734f522fa449f
Andy Kenney <a.kenney@utoronto.ca>
2023-09-15 08:10:42 -0400

    chg file location for hall rd to local

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from streamlit_folium import folium_static
 
 st.write("Test")
 
-fileName = r"E:\FCFGIS\HallRoad.gpkg"
+fileName = r"HallRoad.gpkg"
 gdf = gpd.read_file(fileName)
 
 m = folium.Map(location = (45.404028, -75.544722), zoom_start = 12)
