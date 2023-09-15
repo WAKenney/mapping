5c5fcc2eb163495056cc680f12ea1a8a0d00dda1
Andy Kenney <a.kenney@utoronto.ca>
2023-09-15 08:49:23 -0400

    add fcn to download data from OneDrive

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import base64
 import geopandas as gpd
 import streamlit as st
 import folium
@@ -6,7 +7,24 @@ from streamlit_folium import folium_static
 
 st.write("Test")
 
-fileName = r"HallRoad.gpkg"
+
+@st.cache_data(show_spinner=False)
+def load_onedrive (onedrive_link):
+        
+    #get the share link for the data file form one drive and past below
+    onedrive_link = 'https://1drv.ms/u/s!Alu-nJHZ-vTw1gZM_jZm6VqK98Ga?e=EK9Z6b'
+
+    # this converts the share link from above to a file name readable by pandas etc.
+    data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
+    data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
+    resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
+    return resultUrl
+
+fileName = load_onedrive(onedrive_link)
+
+
+
+
 gdf = gpd.read_file(fileName)
 
 m = folium.Map(location = (45.404028, -75.544722), zoom_start = 12)
