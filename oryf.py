115c1e2d11a8c165c8c548aea1d7e348d0cd4391
WAKenney <80290200+WAKenney@users.noreply.github.com>
2023-10-16 13:03:55 +0000

    First commit

---
+++ oryf.py
@@ -0,0 +1,159 @@
+import base64
+import folium
+from folium.plugins import Fullscreen
+import geopandas as gpd
+import pandas as pd
+import streamlit as st
+from streamlit_folium import folium_static
+
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+
+
+st.header("Priorities")
+screen1 = st.empty()
+screen1.subheader("Loading map data...  Be patient this will take a while!")
+
+
+#get the share link for the data file form one drive and past below
+onedrive_link = 'https://1drv.ms/u/s!Alu-nJHZ-vTw8wUirrIMsP5SxVPS?e=nyljiN'
+
+
+@st.cache_data(show_spinner=True)
+def getData():
+    
+    def load_onedrive (onedrive_link):
+            
+        # this converts the share link from above to a file name readable by pandas etc.
+        data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
+        data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
+        resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
+        return resultUrl
+
+    fileName = load_onedrive(onedrive_link)
+
+    gdf = gpd.read_file(fileName)
+
+    return gdf
+
+gdf = getData()
+
+select_gdf = gdf
+
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    screen1.subheader("Setting up the map...")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    st.download_button("Download filtered data as a csv file",
+                       df.to_csv().encode('utf-8'),
+                       file_name = "Filtered_data.csv",
+                       mime = 'text/csv')
+
+
+    return df
+
+filtdf = filter_dataframe(gdf) 
+
+def mapIt(df):
+# Create and show the map
+    df.to_json()
+    m = folium.Map(location = (45.404028, -75.544722), zoom_start = 12)
+
+    gjson = folium.GeoJson(df, name = 'Priorities').add_to(m)
+
+    folium.GeoJsonTooltip(fields = ["PIN", "Ownership", "Hectares", "Priority", 
+                                    "Ag(%)", "Is(%)", "Vacant(%)", "AgCapabili"]).add_to(gjson)
+
+    #have an ESRI satellite image as an optional base map
+    folium.TileLayer(
+        tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr = 'Esri',
+        name = 'Satellite',
+        overlay = False,
+        control = True
+       ).add_to(m)
+    
+    # add a fullscreen option and layer control to the map
+    Fullscreen().add_to(m)
+    folium.LayerControl().add_to(m)
+
+    folium_static(m)
+
+mapIt(filtdf)
+screen1.empty()
