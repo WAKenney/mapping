485afeeebb2868093bfca0c779f23bf41094279c
WAKenney <80290200+WAKenney@users.noreply.github.com>
2023-10-26 15:11:47 +0000

    CHanged title to ORYF from ORYP(?)

---
+++ 1_ORYF_Mapping.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import base64
+
+ss=st.session_state
+
+st.title('Utilites')
+
+st.subheader('Pick a utility from the sidebar')
